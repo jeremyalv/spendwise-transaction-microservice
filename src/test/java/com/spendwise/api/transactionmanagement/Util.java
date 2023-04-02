9195a718f7b9a77874e664e01201695a6b819494
Jeremy Alva Prathama <jeremyalvax@gmail.com>
2023-04-02 23:48:08 +0700

    fixed bug in test

--- src/test/java/com/spendwise/api/transactionmanagement/Util.java
+++ src/test/java/com/spendwise/api/transactionmanagement/Util.java
@@ -2,10 +2,11 @@ package com.spendwise.api.transactionmanagement;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class Util {
     public static String mapToJson(Object obj) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
         return objectMapper.writeValueAsString(obj);
     }
 }
